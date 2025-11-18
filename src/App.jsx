f72909f76eb08a3a24f6ddc062d1899bf64fcbbb
elphie-pro <onazjr2006@gmail.com>
2025-11-18 09:02:15 +0100

    correct p

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Route,  createRoutesFromElements, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Homepage />} />
+    )
+  )
+
+  return (
+    <>
+     <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+  console.log('Homepage Loaded')
+  return (
+    <div>
+      <h2 style={{ color: "red", fontSize: "40px" }}>We are coconut milk</h2>
+    </div>
+  )
+}
+
+export default Homepage
