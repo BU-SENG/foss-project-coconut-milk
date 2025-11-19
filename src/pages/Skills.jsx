7c7dd99859e281c2313df908095c2d3ea90a7d7d
Oluwafirelade <oluwafireladeorimolade@gmail.com>
2025-11-19 15:20:48 +0100

    update

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -181,7 +181,7 @@ export default function BrowseSkills() {
               />
             </div>
 
-            {/* Category Filter */}
+            
             <div className="md:w-64">
               <div className="relative">
                 <Filter className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
