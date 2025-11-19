5df5759fa367bd2b6053dc902ac29eee34590bd6
Oluwafirelade <oluwafireladeorimolade@gmail.com>
2025-11-19 15:21:12 +0100

    update

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -208,7 +208,7 @@ export default function BrowseSkills() {
           </p>
         </div>
 
-        {/* Skills Grid */}
+       
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredSkills.map(skill => (
             <div
