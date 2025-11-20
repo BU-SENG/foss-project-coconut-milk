d466fe0bdd636fef2250ced47dd53f69bd0de7ea
elphie-pro <onazjr2006@gmail.com>
2025-11-20 02:32:01 +0100

    lik

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/foss-project-coconut-milk/',
   plugins: [react(), tailwindcss()],
 })
